8f2a6e461fd964c84c27395fcbf72f4999174d82
KausaChangs <changayakausa@gmail.com>
2024-03-25 18:14:11 +0200

    created and printed out elements of an array

--- CppCrashCourse.cpp
+++ CppCrashCourse.cpp
@@ -4,7 +4,21 @@ using namespace std;
 
 int main()
 {
-   cout << "Hello World!\n";
+
+    string username = "Kausa Changaya";
+
+    cout << "Hello World! from \n";
+
+    int arr[] = { 1, 2, 3, 4, 5 };
+    int n = sizeof(arr) / sizeof(arr[0]); // Calculate the number of elements in the array
+
+    cout << "Array elements: ";
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 
     system("pause>0");
 
